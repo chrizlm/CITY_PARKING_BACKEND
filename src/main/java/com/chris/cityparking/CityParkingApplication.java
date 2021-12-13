3348eb922b617b686f469829415368415f3595fe
Christopher Lijoodi <christopherlijoodi@Power.local>
2021-12-13 11:46:34 +0300

    first commit

---
+++ src/main/java/com/chris/cityparking/CityParkingApplication.java
@@ -0,0 +1,13 @@
+package com.chris.cityparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityParkingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/chris/cityparking/services/MotoristDetailService.java
@@ -0,0 +1,52 @@
+package com.chris.cityparking.services;
+
+import com.chris.cityparking.exceptions.MotoristNotFoundException;
+import com.chris.cityparking.modules.MotoristDetails;
+import com.chris.cityparking.repositories.MotoristDetailsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MotoristDetailService {
+    @Autowired
+    MotoristDetailsRepo motoristDetailsRepo;
+
+
+    /*
+    getting details
+    saving details
+    update details
+    deleting details
+     */
+
+    public void createMotoristDetail(MotoristDetails motoristDetails){
+        motoristDetailsRepo.save(motoristDetails);
+    }
+
+    public MotoristDetails getMotorist(Long motoristID){
+        return motoristDetailsRepo.getById(motoristID);
+    }
+
+    public List<MotoristDetails> getAllMotorists(){
+        return motoristDetailsRepo.findAll();
+    }
+
+    public void updateMotorist(MotoristDetails motoristDetails){
+        motoristDetailsRepo.findById(motoristDetails.getMotoristID()).ifPresentOrElse(motorist ->{
+            motorist.setFirstName(motoristDetails.getFirstName());
+            motorist.setLastName(motoristDetails.getLastName());
+            motorist.setEmail(motoristDetails.getEmail());
+            motorist.setGender(motoristDetails.getGender());
+            motorist.setPassword(motoristDetails.getPassword());
+            motoristDetailsRepo.save(motorist);
+        }, () ->{
+            throw new MotoristNotFoundException(motoristDetails.getMotoristID());
+        });
+    }
+
+    public void deleteMotorist(Long motoristID){
+        motoristDetailsRepo.deleteById(motoristID);
+    }
+}
